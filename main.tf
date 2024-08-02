3a1bcd782dcd73a928bf9f4c05f52d1d4fabf857
Suresh Kumar Maurya <sureshmaurya811568@gmail.com>
2024-08-03 00:48:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -318,7 +318,7 @@ resource "aws_autoscaling_group" "jenkins_asg" {
   max_size             = var.max_size
   min_size             = var.min_size
   vpc_zone_identifier  = [aws_subnet.private_1.id, aws_subnet.private_2.id]
-  target_group_arns    = [aws_lb_target_group.jenkins_tg.arn]
+  /*target_group_arns    = [aws_lb_target_group.jenkins_tg.arn]*/
   launch_template {
     id      = aws_launch_template.jenkins_launch_template.id
     version = "$Default"
