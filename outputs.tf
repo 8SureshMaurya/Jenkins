bf8618f05364c35018ad468d18b412781c810ae1
Suresh Kumar Maurya <sureshmaurya811568@gmail.com>
2024-08-08 13:20:22 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -22,8 +22,9 @@ output "jenkins_lb_dns" {
   description = "DNS name of the Jenkins load balancer"
   value       = aws_lb.jenkins_lb.dns_name
 }
-
+/*
 output "Autoscaling_group_name" {
   description = "The name of the Auto Scaling group"
   value       = aws_autoscaling_group.jenkins_asg.name
 }
+*/
